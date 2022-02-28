bd94a19489fe1b2e4f429e351faf5a4df59ea29f
your name <your@email.com>
2022-02-28 13:25:44 -0300

    Se agrego un Router

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const morgan = require('morgan');
 app.use(express.json()); // Middleware
 app.use(morgan('dev'));
 
+
 app.use((req, res, next) => { // Se define un middleware global antes de los route handlers
     console.log("Hola desde el middleware");
     next();
@@ -19,15 +20,9 @@ app.use((req, res, next) => { // Se define un middleware para saber el horario d
     next();
 })
 
-// app.get('/', (req, res) => {
-//     res
-//         .status(200)
-//         .json({ message: "Hola mundo", app: "Turismo" });
-// })
 
-// app.post('/', (req, res) => {
-//     res.send("Enviando datos");
-// })
+
+// START SERVER
 
 const port = 3000;
 app.listen(port, () => {
@@ -137,3 +132,117 @@ app.post('/api/v1/tours', createTour);
 app.patch('/api/v1/tours/:id', updateTour);
 
 app.delete('/api/v1/tours/:id', deleteTour);
+
+
+
+// USERS 
+
+const getAllUsers = (req, res) => {
+
+    console.log(req.requestTime);
+    res.status(200).json({
+        status: 'success',
+        time: req.requestTime,
+        data: {
+            users
+        }
+    });
+};
+
+
+const getUser = (req, res) => {
+
+    console.log(req.params);
+    const id = req.params.id * 1;
+
+
+    if (id > users.length) { // Valida si se supera el largo del array
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Id invalido'
+        });
+    }
+
+    const user = users.find(el => el.id === id);
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            user
+        }
+    });
+};
+
+
+const createUser = (req, res) => {
+    const newId = users[users.length - 1].id + 1; // Toma el ultimo tour del array
+
+    const newUser = Object.assign({ id: newId }, req.body);
+    users.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/users.json`, JSON.stringify(users), err => {
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour: newUser
+            }
+        });
+    });
+
+};
+
+const updateUser = (req, res) => {
+
+    if (req.params.id * 1 > users.length) { // Valida si se supera el largo del array
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Id invalido'
+        });
+    }
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            tour: '<Usuario actualizado...>'
+        }
+    });
+};
+
+
+const deleteUser = (req, res) => {
+
+    if (req.params.id * 1 > users.length) { // Valida si se supera el largo del array
+        return res.status(404).json({
+            status: 'fail',
+            message: 'Id invalido'
+        });
+    }
+
+    res.status(204).json({
+        status: 'success',
+        data: null
+    });
+};
+
+
+
+// ROUTERS
+
+
+const userRouter = express.Router(); // ROUTER
+
+
+
+userRouter
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+userRouter
+    .route('/:id')
+    .get(getUser)
+    .patch(updateUser)
+    .delete(deleteUser);
+
+
+app.use('/api/v1/users', userRouter);
