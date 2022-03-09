13d7e80e1f16bd2ea8a9fc27039ae494c762032b
your name <your@email.com>
2022-03-09 14:18:27 -0300

    Se instalo Eslint

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: './config.env' }); // Lee las variables de entorno del archivo config.env
+
+// console.log(process.env);
+
+
 
 // START SERVER
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Servidor activo en el puerto: ${port}`);
 })
