d4368450ac4b9464821cd2e1f25ae68cbb55ba64
your name <your@email.com>
2022-03-15 16:00:28 -0300

    Se agrego Conexion a base de MongoDb

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' }); // Lee las variables de entorno del archivo config.env
 
 // console.log(process.env);
+
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose.connect(DB, { // Conexion a la base de MongoDB
+    useNewUrlParser: true,
+    //  useCreateIndex: true,
+    //  useFindAndModify: false,
+    useUnifiedTopology: true
+
+}).then(() => {
+    console.log('Conectado a la base de la nube!');
+
+}).catch((err) => {
+    console.log(err);
+});
+
+
 
 
 
