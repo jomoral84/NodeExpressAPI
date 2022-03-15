61b01f15d9049c5e8d7817f32790fcc2eb1ff0ce
your name <your@email.com>
2022-03-15 16:40:41 -0300

    Se agrego Conexion a base de MongoDb 2

--- server.js
+++ server.js
@@ -21,7 +21,24 @@ mongoose.connect(DB, { // Conexion a la base de MongoDB
     console.log(err);
 });
 
+const tourSchema = new mongoose.Schema({ // Esquema basico de mongoose
+    name: {
+        type: String,
+        required: [true, "Debe tener un nombre"], // Valida los datos
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: [true, "Debe tener un precio"]
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    }
+})
 
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 
 
