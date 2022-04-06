2692c9e0cb5593561c5ddb9705bdc3682566a8f2
your name <your@email.com>
2022-04-06 16:26:08 -0300

    Refactorizaion de codigo

--- dev-data/routes/tourRoutes.js
+++ dev-data/routes/tourRoutes.js
@@ -9,10 +9,16 @@ const router = express.Router();
 
 // router.param('id', tourController.checkID);
 
+
+
+
 router
     .route('/')
     .get(tourController.getAllTours)
     .post(tourController.createTour);
+
+router
+    .route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
 router
     .route('/:id')
