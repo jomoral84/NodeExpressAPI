8fe4438af5b76076d1513f3e309f19e0d2a19007
your name <your@email.com>
2022-07-27 16:40:09 -0300

    Update and Delete User

--- dev-data/controllers/userController.js
+++ dev-data/controllers/userController.js
@@ -1,5 +1,18 @@
 const APIFeatures = require('../utils/apiFeatures');
 const User = require('../models/userModel');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => { // Variable que filtra los campos que deben permancecer
+    const newObj = {};
+    Object.keys(obj).forEach(el => {
+        if (allowedFields.includes(el)) {
+            newObj[el] = obj[el]
+        }
+    })
+
+    return newObj;
+}
+
 
 
 exports.getAllUsers = async(req, res) => {
@@ -28,6 +41,45 @@ exports.getAllUsers = async(req, res) => {
         });
     }
 };
+
+
+exports.updateMe = async(req, res, next) => {
+    // 1) Crear error si el usuario postea informacion de su password
+    if (req.body.password || req.body.passwordConfirm) {
+        return next(new AppError('Esta ruta no es para cambio de password. Usar UpdateMypassword'), 400);
+
+    }
+
+    // 2) Filtra los campos que no deben ser modificados ejemplo: role
+    const filteredBody = filterObj(req.body, 'name', 'email');
+
+    // 3) Update user data
+    const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+        new: true,
+        runValidators: true
+    })
+
+
+    res.status(200).json({
+        status: 'success',
+        message: 'Cambios aplicados',
+        user: updatedUser
+    });
+}
+
+
+
+exports.deleteMe = async(req, res, next) => {
+    await User.findByIdAndUpdate(req.user.id, { active: false })
+
+    res.status(204).json({
+        status: 'success',
+        message: 'Usuario Borrado',
+        data: null
+    });
+
+}
+
 
 exports.getUser = (req, res) => {
     res.status(500).json({
--- dev-data/routes/userRoutes.js
+++ dev-data/routes/userRoutes.js
@@ -9,6 +9,10 @@ router.post('/signup', authController.signup); // Route para signup
 router.post('/login', authController.login); // Route para login
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+router.patch('/updateMyPassword', authController.protect, authController.updatePassword);
+router.patch('/updateMe', authController.protect, userController.updateMe);
+router.delete('/deleteMe', authController.protect, userController.deleteMe);
+
 
 router
     .route('/')
